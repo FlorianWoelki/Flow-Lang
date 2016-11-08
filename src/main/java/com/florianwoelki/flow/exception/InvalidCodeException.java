2919c59f01d56e530ebd8f7505962cc569c173a6
sgtflow <sgtflow.yt@gmail.com>
2016-11-08 15:50:29 +0100

    Added InvalidCodeException

---
+++ src/main/java/com/florianwoelki/flow/exception/InvalidCodeException.java
@@ -0,0 +1,12 @@
+package com.florianwoelki.flow.exception;
+
+/**
+ * Created by Florian Woelki on 08.11.16.
+ */
+public class InvalidCodeException extends Exception
+{
+    public InvalidCodeException(String message)
+    {
+        super(message);
+    }
+}
