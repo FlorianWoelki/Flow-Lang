a10f734124d7279ee7e0e03fe77e58a5c9b39bda
sgtflow <sgtflow.yt@gmail.com>
2016-11-08 16:58:29 +0100

    Updated FlowLang for running

--- src/main/java/com/florianwoelki/flow/FlowLang.java
+++ src/main/java/com/florianwoelki/flow/FlowLang.java
@@ -1,5 +1,9 @@
 package com.florianwoelki.flow;
 
+import com.florianwoelki.flow.gui.IDE;
+
+import javax.swing.*;
+
 /**
  * Created by Florian Woelki on 08.11.16.
  */
@@ -7,6 +11,12 @@ public class FlowLang
 {
     public static void main(String[] args)
     {
+        Thread.setDefaultUncaughtExceptionHandler((thread, e) ->
+        {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+        });
 
+        new IDE();
     }
 }
